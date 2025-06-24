7a742cdf92165ea7bcb0f089f71ada9f49fae2db
yakov <dunetix@gmail.com>
2025-06-24 20:48:16 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("solaya-cache").then(cache => {
+      return cache.addAll([".", "index.html", "manifest.json", "icon.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
