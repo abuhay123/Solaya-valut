0a06e70bd50baf81682a6f97b421f3f6c76d60a9
yakov <dunetix@gmail.com>
2025-06-25 20:51:39 +0300

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,7 +1,14 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("solaya-cache").then(cache => {
-      return cache.addAll([".", "index.html", "manifest.json", "icon.png"]);
+      return cache.addAll([
+        "index.html",
+        "icon.png",
+        "apple-touch-icon.png",
+        "favicon-96x96.png",
+        "favicon.ico",
+        "site.webmanifest"
+      ]);
     })
   );
 });
